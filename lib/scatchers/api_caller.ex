f5e6261d5ec8854c2a5f4fa99632c92514bbfbb0
Song <skyusang@amazon.com>
2017-10-21 12:13:37 -0700

    update request header

--- lib/scatchers/api_caller.ex
+++ lib/scatchers/api_caller.ex
@@ -3,7 +3,7 @@ defmodule Scatchers.APICaller do
   def pull_search_result do
     # IO.puts "#{DateTime.utc_now()} :: API call start"
     # url = "https://www.mercari.com/jp/search/?keyword=%E3%83%99%E3%82%A2%E3%83%96%E3%83%AA%E3%83%83%E3%82%AF"
-    url = "https://us.hideproxy.me/go.php?u=2pH0B8mZc9id3TuBeFVsHUB6K63JSxqEsvBpeZelv7pb3U0YcYZwiNzmn0x%2B0YAb%2BePB5krqb8XNMyGU4IZfzzHroLe4BYqrpPtTEqW8s%2F%2FOa7KlJCCkivrhtFsW&b=5&f=norefer" 
+    url = "https://us.hideproxy.me/go.php?u=PUUhtIF1t%2BV09aLxElOxsyNBxJcbqkhc3EkBE27RPXjr%2FN3C3hnU0RWrUb5MkWXA9uwjEVsn0CPROb7XPf3GfwV%2BOAigki8EOpbJRdxMhCGas6QUj1yBOdoruCY0&b=5&f=norefer" 
     headers = ["accept-encoding": "gzip, deflate, br",
                "accept-language": "en-US,en;q=0.8,ko;q=0.6",
                "upgrade-insecure-requests": "1",
@@ -11,13 +11,13 @@ defmodule Scatchers.APICaller do
                "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
                "cache-control": "max-age=0",
                "authority": "us.hideproxy.me",
-               "cookie": "s=mc65fv1dpmfk06car55v3sbus4; c[nr-data.net][/][JSESSIONID]=9b46651169277b67%21SEC; c[mercari.com][/][merCtx]=%22%22; _ga=GA1.2.1790029790.1507523816; _gid=GA1.2.849435620.1507523816; _gat=1"
+               "cookie": "s=7rk290n5ur134pbmqi0gicoe81; c[nr-data.net][/][JSESSIONID]=bca5ff77fa9c29a7%21SEC; _ga=GA1.2.1790029790.1507523816; _gid=GA1.2.508547106.1508612315; c[mercari.com][/][merCtx]=%22%22"
               ]
 
     {:ok, resp} = HTTPoison.get(url, headers, [timeout: 50_000, recv_timeout: 50_000])
     res = Floki.find(resp.body, ".items-box")
     |> Enum.map(fn x -> Floki.find(x, "a") end)
-    # |> IO.inspect
+    |> IO.inspect
   end
 
   def translate_to_korean(txt) do
